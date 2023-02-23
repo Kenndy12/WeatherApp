ff5a9161da006b4d5049f22fd615ddd7c4c5ce97
Kenndy <justkenndy92@gmail.com>
2023-02-23 14:47:40 +0700

    2nd change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import getWeatherData from "./api";
+import coldBg from "./assets/cold.jpg";
+import hotBg from "./assets/hot.jpg";
 
 function App() {
 
@@ -11,8 +13,29 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <p>test</p>
+    <div className="app" style={{backgroundImage:`url(${coldBg})`}}>
+      <div className="overlay"> 
+        <div className="container">
+          <div className="section section_inputs">
+            <input type="text" name="city" placeholder="Enter the city"></input>
+            <button>°F</button>
+          </div>
+
+          <div className="section section_temperature">
+            <div className="icon">
+              <h3>London, GB</h3>
+              <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="Weather Icon"></img>
+              <h3>Few Clouds</h3>
+            </div>
+
+            <div className="temperature">
+              <h1>26°C</h1>
+            </div>
+          </div>
+
+          {/* Bottom Description */}
+        </div>
+      </div>
     </div>
   );
 }
